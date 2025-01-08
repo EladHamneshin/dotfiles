31387bd45b7fab164a583ba9d9046f704c98db7c
Elad Hamneshin <hamneshin123@gmail.com>
2025-01-09 00:41:51 +0200

    removed unused pkgs

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,3 @@
-#ask user if they want to install zsh
-read -p "Do you want to install zsh? (y/n): " install_zsh
-if [ "$install_zsh" = "y" ]; then
-    echo "installing zsh..."
-    sudo apt install -y zsh
-    echo "changing default shell to zsh..."
-    chsh -s $(which zsh)
-    zsh
-    echo "## Remember not to switch to back bash before uninstalling zsh." \
-    | gum format
-fi
-
 #install zinit
 echo "installing zinit..."
 curl -fsSL https://git.io/zinit-install | sh
